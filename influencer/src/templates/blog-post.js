94eeed1634e16af78c86711da015f827a54461c4
apecson <aspecson@gmail.com>
2019-01-29 21:20:29 -0500

    Hiding index hero

--- influencer/src/templates/blog-post.js
+++ influencer/src/templates/blog-post.js
@@ -3,9 +3,11 @@ import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
+import classNames from 'classnames'
 import Layout from '../components/layout'
 
 import heroStyles from '../components/hero/hero.module.css'
+import styles from './blog-post.module.css'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -19,23 +21,28 @@ class BlogPostTemplate extends React.Component {
           <div className={heroStyles.hero}>
             <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
           </div>
-          <div className="wrapper">
-            <h1 className="section-headline">{post.title}</h1>
-            <p
-              style={{
-                display: 'block',
-              }}
-            >
-              {post.publishDate}
-            </p>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: post.body.childMarkdownRemark.html,
-              }}
-            />
-          </div>
+          <main className={classNames('wrapper', styles.main)}>
+            <div className={styles.content}>
+              <h1 className="section-headline">{post.title}</h1>
+              <p
+                style={{
+                  display: 'block',
+                }}
+              >
+                {post.publishDate}
+              </p>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: post.body.childMarkdownRemark.html,
+                }}
+              />
+            </div>
+            <div className={styles.sidebar}>
+              I am a sidebar
+            </div>
+          </main>
         </div>
-      </Layout>
+      </Layout >
     )
   }
 }
